62c28063970a65764a4a2f7a3dba3ffba6819d2a
Gagogaa <gregoryjmann26@gmail.com>
2016-10-26 16:00:08 -0400

    Almost done with lab 9

--- Lab-9/cookie.js
+++ Lab-9/cookie.js
@@ -3,8 +3,8 @@ lab 9
 using the module format discussed in class create a module for handling cookies.
 
 - there should be only three public functions for manipulating cookies
-  - void setData(name, value[, numDays = 0])
-  - String getData(name)
+  x void setData(name, value[, numDays = 0])
+  x String getData(name)
   - Number length()
 
 notes:
@@ -26,14 +26,14 @@ function Cookie(){
 
 	// regex constructor https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
   function getData(name){
-    var name = name;
-		var re = name + "=(\\w*)[;]?"; 
+		var re = name + "=(\\w*)"; 
 		re = new RegExp(re);
     return document.cookie.match(name) ? re.exec(document.cookie)[1] : null;
   }
 
 	// I dont think that this is 100% proper but it works
 	// if the cookie only has one item in it split returns an array with blank string that has a length of 1
+	// The _gat (google analyitics cookie) is not returned with Document.cookie despite it being visable in firebug (the normal _ga cookie is visable)
   function length(){
 		if(document.cookie == ""){return 0;}
 		return document.cookie.split(";").length;
@@ -49,16 +49,13 @@ function Cookie(){
 
   // UTC string https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toUTCString
 	// Date() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-	// TODO: needs some work to get working right if the number of day sets it over 31
   function getDate(days){
     var todaysDate = new Date();
 		var year = todaysDate.getUTCFullYear();
 		var month = todaysDate.getUTCMonth();
 		var day = todaysDate.getUTCDate() + days;
-		while(day > ){
 
-		}
-
+		// The Date() function suto corrects dates like Oct 32 to Nov 1
 		var returnDate = new Date(year, month, day);
     return returnDate.toUTCString();
   }
