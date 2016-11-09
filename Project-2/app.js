cd7f7a9e7830e8e0141c6ff461d54741acad6224
Gagogaa <gregoryjmann26@gmail.com>
2016-11-09 12:14:19 -0500

    did work on player movement

--- Project-2/app.js
+++ Project-2/app.js
@@ -11,7 +11,6 @@ function Game(table){
       _move,
       _draw = new Draw(table),
       obj = {
-        move: move,
         start: start
       };
 
@@ -26,20 +25,21 @@ function Game(table){
         _grid[i][j] = "g";
       }
     }
-    //console.log(_grid.join('\n'));
-    //console.log(_grid);
-    //setInterval(gameTick, 100);
+    setInterval(gameTick, 100);
   }
 
 // randomly spawn fires and spawn water when nessisary
+//JSON Deep copy hack found on http://stackoverflow.com/questions/3978492/javascript-fastest-way-to-duplicate-an-array-slice-vs-for-loop
   function gameTick(){
     gameCheck();
-    _draw.draw(_grid, _playerPos);
+    var newarr = JSON.parse(JSON.stringify(_grid));
+    _draw.draw(newarr, _playerPos);
   }
 
 // Checks to see if two items are overlapping and takes the nessisary action
   function gameCheck(){
     if(_grid[_playerPos[0]][_playerPos[1]] === "f"){
+      
     }else if(_grid[_playerPos[0]][_playerPos[1]] === "w"){
 
     }
@@ -60,34 +60,40 @@ function Game(table){
 
   }
 
-  function move(dir){
-    switch (dir) {
-      case 119: //w
+//Key stuff found on https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
+  window.addEventListener("keydown", function (event){
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+
+    switch (event.key) {
+      case "w": //w
+        if(_playerPos[0] > 0){
+          _playerPos[0] = _playerPos[0] -1;
+        }
+        break;
+
+      case "a": //a
         if(_playerPos[1] > 0){
           _playerPos[1] = _playerPos[1] -1;
         }
         break;
 
-      case 97: //a
-        if(_playerPos[1] > 0){
-          _playerPos[1] = _playerPos[1] -1;
+      case "s": //s
+        if(_playerPos[0] < _height -1){
+          _playerPos[0] = _playerPos[0] +1;
         }
         break;
 
-      case 115: //s
-        if(_playerPos[1] > 0){
+      case "d": //d
+        if(_playerPos[1] < _width -1) {
           _playerPos[1] = _playerPos[1] +1;
-        }
-        break;
-
-      case 100: //d
-        if(_playerPos[1] > 0){
-          _playerPos[1] = _playerPos[1] -1;
         }
         break;
     }
     gameCheck();
-  }
+  });
+
   return obj;
 }
 
@@ -118,7 +124,8 @@ function Draw(){
 
   function draw(worldArray, playerPosArray){
     worldArray[playerPosArray[0]][playerPosArray[1]] = "p";
-    console.log(worldArray.join('\n'));
+    document.querySelector("#test").innerHTML = worldArray.join('<br />');
+    //console.log(worldArray.join('\n'));
 
   }
 
